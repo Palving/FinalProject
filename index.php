a1f8526a535d0a1857aab8c0b97fa090d8d5750c
bjornar bjorkmo <bjornarbjorkmo@gmail.com>
2017-11-26 19:51:09 +0100

    La til en beskrivelse av bildene vi brukte, satt language taggen til engelsk å prøvde å gjøre koden litt enklere å lese.

--- index.php
+++ index.php
@@ -1,86 +1,70 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>[EncryptPic]</title>
-        <link rel="stylesheet" href="stylesheetIndex.css">
-    </head>
-    <body>
-       
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+
+	<title>[EncryptPic]</title>
+
+<link rel="stylesheet" href="stylesheetIndex.css">
+</head>
+
+<body>
+    <ul class="meny">
+        <li><a href="side">Using the app</a></li>
+        <li><a href="index.php"> <img src="bilder/lock.png" width="80px" height="80px" alt="An image of the logo of EncryptPic."/></a></li>
+        <li><a href="side">Download</a></li>  
+    </ul>
         
-        <ul class="meny">
-            <li><a href="side">Using the app</a></li>
-           <li><a href="index.php"> <img src="bilder/lock.png" width="80px" height="80px" alt=""/></a></li>
-          
-            <li><a href="side">Download</a></li>
-           
-        </ul>
-        
-          <ul class="bookmarks">
-              <a id="goto">//Bookmarks</a>
+    <ul class="bookmarks">
+        <a id="goto">//Bookmarks</a>
             <li><a href="#intro">Intro</a>
-                 <li><a href="#problem">The problem</a>
-                 <li><a href="#solution">The solution</a>
-        </ul>
+            <li><a href="#problem">The problem</a>
+            <li><a href="#solution">The solution</a>
+    </ul>
         
-        <div class="tittel"
-             <h1><a id="intro"> EncryptPic</a></h1>
-        </div>
-         
-        <div class="forside"
-             
-        <div id="boxIntro">
-            <p> Have you ever been worried that your newly taken photographs gets stored some place you feel like you have no control over?
-            <br><br>With EncryptPic you'll be given a complete oversight over your files' security and location, so you wont have to worry about unwanted eyes taking a peek at your most sensitive material.
-            </p>
-        
-        
-        <div id="boxIntroBilde">
-            <img src="bilder/bilde.png" width="540px" height="392px" alt=""/>
-        
-             
+    <div class="tittel">
+        <h1><a id="intro"> EncryptPic</a></h1>
     </div>
+             
+    <div id="boxIntro">
+        <p> Have you ever been worried that your newly taken photographs gets stored some place you feel like you have no control over?
+        <br><br>With EncryptPic you'll be given a complete oversight over your files' security and location, so you wont have to worry about unwanted eyes taking a peek at your most sensitive material.
+        </p>
+
+        
+        
+    <div id="boxIntroBilde">
+        <img src="bilder/bilde.png" width="540px" height="392px" alt="An image of EncryptPic integrated into the camera app."/> 
+   	</div>
             
-            <h1><a id="problem"> (The problem)</a></h1>
-            <div class="innhold"
-                <div id="boxProblem"> 
-                    <p> In today's society there is a lot of talk about files and sensitive material being hacked, but not enough on how to prevent it. 
-                    This has caused a technological ignorance in the average person on how filestoring and security works. 
-                    The average person uses a fair amount of technology in their daily lives, and they put their trust in their products' basic security measures.
-                    </p>
-                 
-                 
+    <h1><a id="problem">(The problem)</a></h1>
+
+    <div class="innhold">
+        <div id="boxProblem"> 
+           	<p> In today's society there is a lot of talk about files and sensitive material being hacked, but not enough on how to prevent it. 
+            This has caused a technological ignorance in the average person on how filestoring and security works. 
+            The average person uses a fair amount of technology in their daily lives, and they put their trust in their products' basic security measures.
+            </p>        
         </div>
-             <div id="boxProblemBilde">
-            <img src="bilder/hack.jpg" width="540px" height="392px" alt=""/>
-        
-             
+
+        <div id="boxProblemBilde">
+            <img src="bilder/hack.jpg" width="540px" height="392px" alt="An image showing encryption of a phone."/>
+        </div>         
     </div>
         
-                 <h1><a id="solution"> (The solution)</a></h1>
-            <div class="innhold"
-                <div id="boxSolution"> 
-                    <p> Our solution provides a safeguard for your files by encrypting them instantly. By encrypting your files, we make it so that they'll only be available to your eyes only, through a password of your choice.
-                        
-                        
-                    </p>
-                 
-                 
-        </div>
-             <div id="boxSolutionBilde">
-            <img src="bilder/hack.jpg" width="540px" height="392px" alt=""/>
-        
-             
+    <h1><a id="solution"> (The solution)</a></h1>
+
+    <div class="innhold">
+        <div id="boxSolution"> 
+            <p>Our solution provides a safeguard for your files by encrypting them instantly. By encrypting your files, we make it so that they'll only be available to your eyes only, through a password of your choice.
+            </p>
+        </div>                 
     </div>
-          
-        <?php
-        // put your code here
-        ?>
-           
-    </body>
+
+    <div id="boxSolutionBilde">
+       	<img src="bilder/hack.jpg" width="540px" height="392px" alt="An image showing encryption of a phone."/>
+    </div>      
+</body>
 </html>
